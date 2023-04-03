94de398771cb4d4e6cdbdb00d4e373cac5f7aee5
Bozhi-Lyu <2455523629@qq.com>
2023-04-04 00:40:51 +0800

    Lab2 finished.

--- lab2/IntList/SquarePrimesTest.java
+++ lab2/IntList/SquarePrimesTest.java
@@ -17,4 +17,28 @@ public class SquarePrimesTest {
         assertEquals("14 -> 15 -> 16 -> 289 -> 18", lst.toString());
         assertTrue(changed);
     }
+
+    @Test
+    public void testSquarePrimes1() {
+        IntList lst = IntList.of(2, 15, 3, 11, 71);
+        boolean changed = IntListExercises.squarePrimes(lst);
+        assertEquals("4 -> 15 -> 9 -> 121 -> 5041", lst.toString());
+        assertTrue(changed);
+    }
+
+    @Test
+    public void testSquarePrimes2() {
+        IntList lst = IntList.of(4, 100, 21, 8, 26);
+        boolean changed = IntListExercises.squarePrimes(lst);
+        assertEquals("4 -> 100 -> 21 -> 8 -> 26", lst.toString());
+        assertFalse(changed);
+    }
+
+    @Test
+    public void testSquarePrimes3() {
+        IntList lst = IntList.of(19, 97, 87, 23, 73);
+        boolean changed = IntListExercises.squarePrimes(lst);
+        assertEquals("361 -> 9409 -> 87 -> 529 -> 5329", lst.toString());
+        assertTrue(changed);
+    }
 }
